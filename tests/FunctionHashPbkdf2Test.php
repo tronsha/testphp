2295e22dc49b89c8b6f3d6108e5e66a035076021
Stefan Hüsges <Tronsha@gmx.de>
2015-03-16 13:35:02 +0100

    Update FunctionHashPbkdf2Test.php

--- tests/FunctionHashPbkdf2Test.php
+++ tests/FunctionHashPbkdf2Test.php
@@ -13,7 +13,7 @@ class FunctionHashPbkdf2Test extends PHPUnit_Framework_TestCase
             $iterations = 1000;
             $salt = mcrypt_create_iv(16, MCRYPT_DEV_URANDOM);
             $hash = hash_pbkdf2("sha256", $password, $salt, $iterations, 20);
-            $this->assertEquals(strlen('120fb6cffcf8b32c43e7'), strlen($hash));
+            $this->assertEquals(20, strlen($hash));
         }
     }
 }
