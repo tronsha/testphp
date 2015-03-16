606634267170670d5c702335142904167e077949
Stefan Hüsges <Tronsha@gmx.de>
2015-03-16 13:33:34 +0100

    Update FunctionHashEqualsTest.php

--- tests/FunctionHashEqualsTest.php
+++ tests/FunctionHashEqualsTest.php
@@ -11,7 +11,7 @@ class FunctionHashEqualsTest extends PHPUnit_Framework_TestCase
         if (version_compare(phpversion(), '5.6.0', '>=') === true) {
             $expected  = crypt('12345', '$2a$07$usesomesillystringforsalt$');
             $correct   = crypt('12345', '$2a$07$usesomesillystringforsalt$');
-            $incorrect = crypt('apple',  '$2a$07$usesomesillystringforsalt$');
+            $incorrect = crypt('apple', '$2a$07$usesomesillystringforsalt$');
             $this->assertTrue(hash_equals($expected, $correct));
             $this->assertFalse(hash_equals($expected, $incorrect));
         }
