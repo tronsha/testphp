6b5355885207a0713aaf6718a2f45614561929e6
Stefan Hüsges <tronsha@gmx.de>
2015-03-17 10:38:04 +0100

    PHP 5.4.0 => The constants ENT_HTML401, ENT_XML1, ENT_XHTML and ENT_HTML5 were added

--- tests/FunctionGetHtmlTranslationTableTest.php
+++ tests/FunctionGetHtmlTranslationTableTest.php
@@ -8,11 +8,13 @@ class FunctionGetHtmlTranslationTableTest extends PHPUnit_Framework_TestCase
 {
     public function testFunctionGetHtmlTranslationTable()
     {
-        $array = get_html_translation_table(HTML_ENTITIES, ENT_QUOTES | ENT_HTML5);
-        $this->assertEquals('&quot;', $array['"']);
-        $this->assertEquals('&num;', $array['#']);
-        $this->assertEquals('&dollar;', $array['$']);
-        $this->assertEquals('&percnt;', $array['%']);
-        $this->assertEquals('&amp;', $array['&']);
+        if (version_compare(phpversion(), '5.4.0', '>=') === true) {
+            $array = get_html_translation_table(HTML_ENTITIES, ENT_QUOTES | ENT_HTML5);
+            $this->assertEquals('&quot;', $array['"']);
+            $this->assertEquals('&num;', $array['#']);
+            $this->assertEquals('&dollar;', $array['$']);
+            $this->assertEquals('&percnt;', $array['%']);
+            $this->assertEquals('&amp;', $array['&']);
+        }
     }
 }
