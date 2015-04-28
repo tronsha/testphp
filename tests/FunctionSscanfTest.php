064df27991a90d77691d623bcaf87a18cccbf171
Stefan Hüsges <tronsha@gmx.de>
2015-04-28 08:01:46 +0200

    sscanf

---
+++ tests/FunctionSscanfTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Class FunctionSscanfTest
+ * @link http://php.net/manual/de/function.sscanf.php
+ */
+class FunctionSscanfTest extends PHPUnit_Framework_TestCase
+{
+    public function testFunctionSscanf()
+    {
+        $this->assertEquals(array('2350001'), sscanf("SN/2350001", "SN/%d"));
+        $this->assertEquals(array('Januar', 1, 2000), sscanf("Januar 01 2000", "%s %d %d"));
+        $auth = "24\tLewis Carroll";
+        $n = sscanf($auth, "%d\t%s %s", $id, $first, $last);
+        $this->assertEquals(24, $id);
+        $this->assertEquals('Lewis', $first);
+        $this->assertEquals('Carroll', $last);
+    }
+}
