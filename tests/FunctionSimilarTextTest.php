a7a943dc88ec92eeb38b4880293171ff506b72a5
Stefan Hüsges <tronsha@gmx.de>
2015-05-01 11:37:46 +0200

    similar_text

---
+++ tests/FunctionSimilarTextTest.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Class FunctionSimilarTextTest
+ * @link http://php.net/manual/de/function.similar-text.php
+ */
+class FunctionSimilarTextTest extends PHPUnit_Framework_TestCase
+{
+    public function testFunctionSimilarText()
+    {
+        $this->markTestIncomplete();
+        //TODO
+    }
+}
