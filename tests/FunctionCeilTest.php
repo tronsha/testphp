230b0f2e39cb04381e007c74fb608aac8da737fe
Stefan Hüsges <tronsha@gmx.de>
2015-05-20 07:39:55 +0200

    ceil

---
+++ tests/FunctionCeilTest.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Class FunctionCeilTest
+ * @link http://php.net/manual/de/function.ceil.php
+ */
+class FunctionCeilTest extends PHPUnit_Framework_TestCase
+{
+    public function testFunctionCeil()
+    {
+        $this->assertEquals(5, ceil(4.3));
+        $this->assertEquals(10, ceil(9.999));
+        $this->assertEquals(-3, ceil(-3.14));
+    }
+}
