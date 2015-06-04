bb954101ec8191842f02e8f5de2c61ce72b3a1ec
Stefan Hüsges <tronsha@gmx.de>
2015-06-04 20:21:27 +0200

    is_finite

---
+++ tests/FunctionIsFiniteTest.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Class FunctionIsFiniteTest
+ * @link http://php.net/manual/en/function.is-finite.php
+ */
+class FunctionIsFiniteTest extends PHPUnit_Framework_TestCase
+{
+    public function testFunctionIsFinite()
+    {
+        $this->markTestIncomplete();
+        //TODO
+    }
+}
