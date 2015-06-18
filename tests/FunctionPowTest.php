6d6c66daea25debde3887da51f7fc74b6bce3f79
Stefan Hüsges <tronsha@gmx.de>
2015-06-18 07:58:54 +0200

    pow

---
+++ tests/FunctionPowTest.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Class FunctionPowTest
+ * @link http://php.net/manual/en/function.pow.php
+ */
+class FunctionPowTest extends PHPUnit_Framework_TestCase
+{
+    public function testFunctionPow()
+    {
+        $this->assertEquals(256, pow(2, 8));
+        $this->assertEquals(1, pow(-1, 20));
+        $this->assertEquals(1, pow(0, 0));
+        $this->assertTrue(is_nan(pow(-1, 5.5)));
+    }
+}
