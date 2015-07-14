f9e8aa6b60c475a352d4601dd405597c84105460
Stefan Hüsges <tronsha@gmx.de>
2015-07-14 19:42:05 +0200

    array_diff_key

---
+++ tests/FunctionArrayDiffKeyTest.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Class FunctionArrayDiffKeyTest
+ * @link http://php.net/manual/en/function.array-diff-key.php
+ */
+class FunctionArrayDiffKeyTest extends PHPUnit_Framework_TestCase
+{
+    public function testFunctionArrayDiffKey()
+    {
+        $array1 = array('blue'  => 1, 'red'  => 2, 'green'  => 3, 'purple' => 4);
+        $array2 = array('green' => 5, 'blue' => 6, 'yellow' => 7, 'cyan'   => 8);
+        $result = array_diff_key($array1, $array2);
+        $output_array = array('red' => 2, 'purple' => 4);
+        $this->assertEquals($output_array, $result);
+    }
+}
