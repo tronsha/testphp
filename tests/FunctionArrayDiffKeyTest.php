d40d15163a92148bd4b656ba17a58ea7c1ece554
Stefan Hüsges <tronsha@gmx.de>
2015-07-15 14:49:48 +0200

    array_diff_key

--- tests/FunctionArrayDiffKeyTest.php
+++ tests/FunctionArrayDiffKeyTest.php
@@ -8,10 +8,14 @@ class FunctionArrayDiffKeyTest extends PHPUnit_Framework_TestCase
 {
     public function testFunctionArrayDiffKey()
     {
-        $array1 = array('blue'  => 1, 'red'  => 2, 'green'  => 3, 'purple' => 4);
-        $array2 = array('green' => 5, 'blue' => 6, 'yellow' => 7, 'cyan'   => 8);
-        $result = array_diff_key($array1, $array2);
-        $output_array = array('red' => 2, 'purple' => 4);
-        $this->assertEquals($output_array, $result);
+        if (version_compare(phpversion(), '5.1.0', '>=') === true) {
+            $array1 = array('blue' => 1, 'red' => 2, 'green' => 3, 'purple' => 4);
+            $array2 = array('green' => 5, 'blue' => 6, 'yellow' => 7, 'cyan' => 8);
+            $result = array_diff_key($array1, $array2);
+            $output_array = array('red' => 2, 'purple' => 4);
+            $this->assertEquals($output_array, $result);
+        } else {
+            $info = phpversion() . ' < 5.1.0';
+        }
     }
 }
