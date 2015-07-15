4db8f740982ecc2d1892a0e4e407ab13643e5291
Stefan Hüsges <tronsha@gmx.de>
2015-07-15 14:52:48 +0200

    array_column

--- tests/FunctionArrayColumnTest.php
+++ tests/FunctionArrayColumnTest.php
@@ -6,31 +6,38 @@
  */
 class FunctionArrayColumnTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @requires PHP 5.5
+     */
     public function testFunctionArrayColumn()
     {
-        $records = array(
-            array(
-                'id' => 2135,
-                'first_name' => 'John',
-                'last_name' => 'Doe',
-            ),
-            array(
-                'id' => 3245,
-                'first_name' => 'Sally',
-                'last_name' => 'Smith',
-            ),
-            array(
-                'id' => 5342,
-                'first_name' => 'Jane',
-                'last_name' => 'Jones',
-            ),
-            array(
-                'id' => 5623,
-                'first_name' => 'Peter',
-                'last_name' => 'Doe',
-            )
-        );
-        $this->assertEquals(array('John', 'Sally', 'Jane', 'Peter'), array_column($records, 'first_name'));
-        $this->assertEquals(array(2135 => 'Doe', 3245 => 'Smith', 5342 => 'Jones', 5623 => 'Doe'), array_column($records, 'last_name', 'id'));
+        if (version_compare(phpversion(), '5.5.0', '>=') === true) {
+            $records = array(
+                array(
+                    'id' => 2135,
+                    'first_name' => 'John',
+                    'last_name' => 'Doe',
+                ),
+                array(
+                    'id' => 3245,
+                    'first_name' => 'Sally',
+                    'last_name' => 'Smith',
+                ),
+                array(
+                    'id' => 5342,
+                    'first_name' => 'Jane',
+                    'last_name' => 'Jones',
+                ),
+                array(
+                    'id' => 5623,
+                    'first_name' => 'Peter',
+                    'last_name' => 'Doe',
+                )
+            );
+            $this->assertEquals(array('John', 'Sally', 'Jane', 'Peter'), array_column($records, 'first_name'));
+            $this->assertEquals(array(2135 => 'Doe', 3245 => 'Smith', 5342 => 'Jones', 5623 => 'Doe'), array_column($records, 'last_name', 'id'));
+        } else {
+            $info = phpversion() . ' < 5.5.0';
+        }
     }
 }
