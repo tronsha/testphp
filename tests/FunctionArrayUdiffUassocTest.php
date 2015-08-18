c634026ca110f0745ae2f09aebb8e58be7f2b57a
Stefan Hüsges <tronsha@gmx.de>
2015-08-18 07:50:50 +0200

    array_udiff_uassoc

--- tests/FunctionArrayUdiffUassocTest.php
+++ tests/FunctionArrayUdiffUassocTest.php
@@ -1,16 +1,16 @@
 <?php
 
 /**
- * Class FunctionArrayUdiffAssocTest
- * @link http://php.net/manual/en/function.array-udiff-assoc.php
+ * Class FunctionArrayUdiffUassocTest
+ * @link http://php.net/manual/en/function.array-udiff-uassoc.php
  */
-class FunctionArrayUdiffAssocTest extends PHPUnit_Framework_TestCase
+class FunctionArrayUdiffUassocTest extends PHPUnit_Framework_TestCase
 {
-    public function testFunctionArrayUdiffAssoc()
+    public function testFunctionArrayUdiffUassoc()
     {
-        $a = array("0.1" => new cr(9), "0.5" => new cr(12), 0 => new cr(23), 1 => new cr(4), 2 => new cr(-15),);
-        $b = array("0.2" => new cr(9), "0.5" => new cr(22), 0 => new cr(3), 1 => new cr(4), 2 => new cr(-15),);
-        $result = array_udiff_assoc($a, $b, array("cr", "comp_func_cr"));
+        $a = array("0.1" => new cr(9), "0.5" => new cr(12), 0 => new cr(23), 1=> new cr(4), 2 => new cr(-15),);
+        $b = array("0.2" => new cr(9), "0.5" => new cr(22), 0 => new cr(3), 1=> new cr(4), 2 => new cr(-15),);
+        $result = array_udiff_uassoc($a, $b, array("cr", "comp_func_cr"), array("cr", "comp_func_key"));
         $this->assertEquals(
             'Array([0.1]=>crObject([priv_member:cr:private]=>9)[0.5]=>crObject([priv_member:cr:private]=>12)[0]=>crObject([priv_member:cr:private]=>23))',
             preg_replace('/\s+/', '', print_r($result, true))
