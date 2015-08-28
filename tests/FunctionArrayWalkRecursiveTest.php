e90279241be7a87ec19207915f17fcee52482db1
Stefan Hüsges <tronsha@gmx.de>
2015-08-28 07:49:25 +0200

    array_walk_recursive

--- tests/FunctionArrayWalkRecursiveTest.php
+++ tests/FunctionArrayWalkRecursiveTest.php
@@ -11,13 +11,13 @@ class FunctionArrayWalkRecursiveTest extends PHPUnit_Framework_TestCase
         $sweet = array('a' => 'apple', 'b' => 'banana');
         $fruits = array('sweet' => $sweet, 'sour' => 'lemon');
         array_walk_recursive($fruits, 'test_print');
-        $this->expectOutputString("a holds apple\nb holds banana\nsour holds lemon\n");
+        $this->expectOutputString("a. apple\nb. banana\nsour. lemon\n");
     }
 }
 
 if (function_exists('test_print') === false) {
     function test_print($item, $key)
     {
-        echo "$key holds $item\n";
+        echo $key . '. ' . $item . "\n";
     }
 }
